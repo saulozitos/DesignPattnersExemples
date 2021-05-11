02dd245775a4750ab7826e66561700966e4b2c4e
Saulo Campos <contatosauloaraujo@gmail.com>
2021-05-10 23:04:04 -0300

    rulo of five

--- Observer/src/iobserver.h
+++ Observer/src/iobserver.h
@@ -4,7 +4,13 @@
 
 class IObserver
 {
+    IObserver(const IObserver&) = delete; // copy constructor
+    IObserver& operator=(const IObserver&) = delete; // copy assignment
+    IObserver(const IObserver&&) = delete; // move constructor
+    IObserver& operator=(IObserver&&) = delete; // move assignment
+
 public:
+    IObserver() = default;
     virtual ~IObserver(){};
     virtual void update(const std::string &msn) = 0;
 };
--- Observer/src/isubject.h
+++ Observer/src/isubject.h
@@ -4,7 +4,13 @@ class IObserver;
 
 class ISubject
 {
+    ISubject(const ISubject&) = delete; // copy constructor
+    ISubject& operator=(const ISubject&) = delete; // copy assignment
+    ISubject(const ISubject&&) = delete; // move constructor
+    ISubject& operator=(ISubject&&) = delete; // move assignment
+
 public:
+    ISubject() = default;
     virtual ~ISubject(){};
     virtual void attach(IObserver *observer) = 0;
     virtual void detach(IObserver *observer) = 0;
